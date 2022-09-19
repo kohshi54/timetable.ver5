51c6edaae581b6e44ff4e058682f99636736e861
kohshi54 <kohshi54.yam@gmail.com>
2022-09-19 15:53:20 +0900

    Initial Commit

---
+++ timetable4icu-git/timetable4icu_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  timetable4icu_gitApp.swift
+//  timetable4icu-git
+//
+//  Created by Kohshi Yamaguchi on 2022/09/19.
+//
+
+import SwiftUI
+
+@main
+struct timetable4icu_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
